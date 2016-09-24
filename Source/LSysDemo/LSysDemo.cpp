367e26b6696f9d34cb62ee59c855864a4566b652
Robert Chubb <paraboliclabs@gmail.com>
2016-09-23 21:56:30 -0400

    Add Example Project to branch

---
+++ Source/LSysDemo/LSysDemo.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LSysDemo.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LSysDemo, "LSysDemo" );
---
+++ Source/LSysDemo/LSysDemoGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LSysDemoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LSYSDEMO_API ALSysDemoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
